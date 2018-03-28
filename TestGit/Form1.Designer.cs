1ed7a80cf92a61c9b9e39c555102c94cf80d0f2c
Лазарев ИМ <superspy2014@gmail.com>
2018-03-28 11:31:48 +0300

    Опять этот Now

--- TestGit/Form1.Designer.cs
+++ TestGit/Form1.Designer.cs
@@ -28,48 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lvTable = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btn1
             // 
-            this.button1.Location = new System.Drawing.Point(588, 224);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Жмак!";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn1.Location = new System.Drawing.Point(12, 473);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Явно";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Btn1_Click);
             // 
-            // label1
+            // lvTable
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(568, 82);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 33);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Before";
+            this.lvTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvTable.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvTable.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvTable.FullRowSelect = true;
+            this.lvTable.GridLines = true;
+            this.lvTable.Location = new System.Drawing.Point(12, 12);
+            this.lvTable.Name = "lvTable";
+            this.lvTable.Size = new System.Drawing.Size(476, 455);
+            this.lvTable.TabIndex = 1;
+            this.lvTable.UseCompatibleStateImageBehavior = false;
+            this.lvTable.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "#";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Время";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "HEX";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "DIFF";
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn2.Location = new System.Drawing.Point(93, 473);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "В фоне";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn3.Location = new System.Drawing.Point(174, 473);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "Слип";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.Btn3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(500, 508);
+            this.Controls.Add(this.lvTable);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.ListView lvTable;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
     }
 }
 
--- TestGit/Form1.cs
+++ TestGit/Form1.cs
@@ -1,28 +1,183 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace TestGit
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() //.ctor 
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        /** https://rsdn.org/forum/winapi/4159851.flat - интересная статья по системным таймерам **/
+
+        private void Btn1_Click(object sender, EventArgs e) //Явно 
         {
-            label1.Text = "After";
-            MessageBox.Show("Hello C#");
-            int i = 1000;
-            this.Text = (++i).ToString();
+            try
+            {
+                btn1.Enabled = false;
+                btn2.Enabled = false;
+                btn3.Enabled = false;
+
+                lvTable.BeginUpdate(); //защита от мерцания контрола
+                lvTable.Items.Clear();
+
+                long Undo = 0, Diff = 0;
+                TimeSpan ts;
+
+                for (int i = 1; i < 26; i++)
+                {
+                    LoadProc(); //дали нагрузку на процессор для увеличения времени работы цикла
+                    var d = DateTime.Now; //получили с чипа текущее время
+                    var lv = lvTable.Items.Add(i.ToString()); //добавили строку в таблицу с номером итерации
+                    lv.SubItems.Add(d.ToString("mm:ss.fffffff")); //2 колонка - время 
+                    lv.SubItems.Add(d.Ticks.ToString("X")); //3 колонка - сериализовнное время
+                    Diff = (Undo == 0) ? 0 : d.Ticks - Undo; //вычислили разницу между строками в тактах
+                    ts = new TimeSpan(Diff);
+                    lv.SubItems.Add($"{Diff.ToString()} ({ts.TotalMilliseconds} мс)"); //4 колонка - разница в тактах от прошлого значения
+                    Undo = d.Ticks; //перезаполнили "прошлое" значение текущим для последующего сравнения
+                    lv.BackColor = (Diff > 0) //фон строки белый для разницы 0, красный для 10000 и оранжевый для 100000
+                        ? (Diff > 11000) ? System.Drawing.Color.Orange : System.Drawing.Color.MediumVioletRed
+                        : System.Drawing.Color.White;
+                }
+
+                foreach (ColumnHeader c in lvTable.Columns) //делаем отображение таблицы удобным
+                {
+                    c.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    var w = c.Width;
+                    c.AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+                    if (c.Width < w) { c.Width = w; } //если столбец будет пуст, его ширина вернется к ширине по заголовку
+                }
+            }
+            finally
+            {
+                lvTable.EndUpdate(); //обязательно вернули прорисовку обратно
+
+                btn1.Enabled = true;
+                btn2.Enabled = true;
+                btn3.Enabled = true;
+            }
+        }
+
+        private void Btn2_Click(object sender, EventArgs e) //В фоне 
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            long Undo = 0, Diff = 0;
+            TimeSpan ts;
+
+            sb.Clear();
+            Task t = Task.Run(() => //вся работы заключена в лямбде, которая работает в отдельном потоке через таск
+            {
+                int i = 1;
+                while (i < 26)
+                {
+                    LoadProc(); //дали нагрузку на процессор для увеличения времени работы цикла
+                    var d = DateTime.Now; //получили с чипа текущее время
+                    Diff = (Undo == 0) ? 0 : d.Ticks - Undo; //вычислили разницу между строками в тактах
+                    sb.AppendLine($"{i.ToString()}|{d.ToString("mm:ss.fffffff")}|{d.Ticks.ToString("X")}|{Diff}"); // код лямбды имеет доступ к локальным переменным вызывающей процедуры
+                    Undo = d.Ticks; //перезаполнили "прошлое" значение текущим для последующего сравнения
+                    i += 1;
+                }
+            });
+            t.Wait();
+
+            try //в качестве обратной связи от таска выбрал строку, которую нужно разделить на строки и столбцы. Существует масса более удобных вариантов, но хотел код посложнее для тренировки
+            {
+                lvTable.BeginUpdate(); //защита от мерцания контрола
+                lvTable.Items.Clear();
+
+                string[] L = sb.ToString().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+                foreach (string NL in L)
+                {
+                    if (string.IsNullOrEmpty(NL)) { continue; }
+                    string[] R = NL.Split(new[] { "|" }, StringSplitOptions.None);
+                    var lv = lvTable.Items.Add(R[0]); //добавили строку в таблицу с номером итерации
+                    lv.SubItems.Add(R[1]); //2 колонка - время 
+                    lv.SubItems.Add(R[2]); //3 колонка - сериализовнное время
+                    Diff = int.Parse(R[3]);
+                    ts = new TimeSpan(Diff);
+                    lv.SubItems.Add($"{R[3]} ({ts.TotalMilliseconds} мс)"); //4 колонка - разница в тактах от прошлого значения
+                    lv.BackColor = (Diff > 0) //фон строки белый для разницы 0, красный для 10000 и оранжевый для 100000
+                        ? (Diff > 11000) ? System.Drawing.Color.Orange : System.Drawing.Color.MediumVioletRed
+                        : System.Drawing.Color.White;
+                }
+
+                foreach (ColumnHeader c in lvTable.Columns) //делаем отображение таблицы удобным
+                {
+                    c.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    var w = c.Width;
+                    c.AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+                    if (c.Width < w) { c.Width = w; } //если столбец будет пуст, его ширина вернется к ширине по заголовку
+                }
+            }
+            finally
+            {
+                lvTable.EndUpdate(); //обязательно вернули прорисовку обратно
+
+                btn1.Enabled = true;
+                btn2.Enabled = true;
+                btn3.Enabled = true;
+            }
+        }
+
+        private void Btn3_Click(object sender, EventArgs e) //Слип 
+        {
+            try
+            {
+                btn1.Enabled = false;
+                btn2.Enabled = false;
+                btn3.Enabled = false;
+
+                lvTable.BeginUpdate(); //защита от мерцания контрола
+                lvTable.Items.Clear();
+
+                long Undo = 0, Diff = 0;
+                TimeSpan ts;
+
+                for (int i = 1; i < 26; i++)
+                {
+                    System.Threading.Thread.Sleep(1); //дали прерывание кванта времени
+                    var d = DateTime.Now; //получили с чипа текущее время
+                    var lv = lvTable.Items.Add(i.ToString()); //добавили строку в таблицу с номером итерации
+                    lv.SubItems.Add(d.ToString("mm:ss.fffffff")); //2 колонка - время 
+                    lv.SubItems.Add(d.Ticks.ToString("X")); //3 колонка - сериализовнное время
+                    Diff = (Undo == 0) ? 0 : d.Ticks - Undo; //вычислили разницу между строками в тактах
+                    ts = new TimeSpan(Diff);
+                    lv.SubItems.Add($"{Diff.ToString()} ({ts.TotalMilliseconds} мс)"); //4 колонка - разница в тактах от прошлого значения
+                    Undo = d.Ticks; //перезаполнили "прошлое" значение текущим для последующего сравнения
+                    lv.BackColor = (Diff > 0) //фон строки белый для разницы 0, красный для 10000 и оранжевый для 100000
+                        ? (Diff > 11000) ? System.Drawing.Color.Orange : System.Drawing.Color.MediumVioletRed
+                        : System.Drawing.Color.White;
+                }
+
+                foreach (ColumnHeader c in lvTable.Columns) //делаем отображение таблицы удобным
+                {
+                    c.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    var w = c.Width;
+                    c.AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+                    if (c.Width < w) { c.Width = w; } //если столбец будет пуст, его ширина вернется к ширине по заголовку
+                }
+            }
+            finally
+            {
+                lvTable.EndUpdate(); //обязательно вернули прорисовку обратно
+            }
+
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+        }
+
+        void LoadProc() //Бесполезная нагрузка 
+        {
+            for (int i = 0; i < 1000000; i++)
+            { var y = i ^ 3; }
         }
     }
 }
